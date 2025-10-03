cda5cde1f8b283f29d24a2a2f637e968e10c84d6
KlaiGhassen <ghassen.klai@esprit.tn>
2025-10-03 15:37:19 +0100

    second commit- prosit1

---
+++ src/ZooManagment.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ZooManagment {
+    public static void main(String[] args) {
+        int nbrCages = 20;
+        String zooName = "my zoo";
+        System.out.println("Zoo Name: " + zooName + " nbrCages: " + nbrCages);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the name of the zoo");
+        zooName = input.nextLine();
+        System.out.println("enter nbr cages: ");
+        nbrCages = input.nextInt();
+
+        System.out.println("Zoo Name: " + zooName + " nbrCages: " + nbrCages);
+
+
+    }
+
+
+}
